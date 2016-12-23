f0eabfec526f38871cf3dfe1cd58499319188de8
shufengw <imgamer@163.com>
2016-12-23 17:23:56 +0800

    update

---
+++ dbx/lib/message_define/DBMessageBuilder.h
@@ -0,0 +1,14 @@
+#ifndef __MESSAGE_BUILDER_H_
+#define __MESSAGE_BUILDER_H_
+
+#include "CSCResultMsg.h"
+
+
+class DBMessageBuilder
+{
+public:
+    CSCResultMsg *BuildCSCResultMsg()
+    {}
+};
+
+#endif // __MESSAGE_BUILDER_H_
