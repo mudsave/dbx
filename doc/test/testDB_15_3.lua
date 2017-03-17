34c21c31675d05cdf5da0753162dbfd318ec2253
shufengw <imgamer@163.com>
2017-03-17 15:01:52 +0800

    1. 测试脚本；2. 正确的测试结果保存

--- doc/test/testDB_15_3.lua
+++ doc/test/testDB_15_3.lua
@@ -1,4 +1,6 @@
-print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+--------------------------------------------------------------------
+--[[
 local params = {{}}
 local function clearParams()
 	local t = params[1]
@@ -6,8 +8,7 @@ local function clearParams()
 		t[k] = nil
 	end
 end
---------------------------------------------------------------------
---[[
+
 	clearParams()
 	params[1]["spName"] = "sp_UpdatePlayer"
 	params[1]["dataBase"] = 1
@@ -40,11 +41,72 @@ end
 	
 ]]
 
-clearParams()
-params[1]["spName"] = "sp_Login"
-params[1]["dataBase"] = 1
-params[1]["sort"] = "account,name,dbid"
-params[1]["account"] = "zgj"
-params[1]["name"] = "1"
-params[1]["dbid"] = 1
-LuaDBAccess.exeSP(params, false)
+--[[
+-- 测试代码
+local function LoginZGJ(callback)
+	clearParams()
+	params[1]["spName"] = "sp_Login"
+	params[1]["dataBase"] = 1
+	params[1]["sort"] = "account,name,dbid"
+	params[1]["account"] = "zgj"
+	params[1]["name"] = "1"
+	params[1]["dbid"] = 1
+	local operationID = LuaDBAccess.exeSP(params, false)
+end
+]]
+
+
+--[[
+-- 测试代码：放置到core/LuaDBAccess.lua模块中
+function LuaDBAccess.LoginZGJ(callbackFunction, callbackArgs)
+	clearParams()
+	params[1]["spName"] = "sp_Login"
+	params[1]["dataBase"] = 1
+	params[1]["sort"] = "account,name,dbid"
+	params[1]["account"] = "zgj"
+	params[1]["name"] = "1"
+	params[1]["dbid"] = 1
+	local operationID = LuaDBAccess.exeSP(params, false)
+	
+	local callback = {
+		func = callbackFunction,
+		args = callbackArgs
+	}
+	DB_CallbackContext[operationID] = callback
+end
+]]
+
+
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+
+local goThroughtTableCount = 0
+local function GoThroughTable(table)
+	for index, record in pairs(table) do
+		if type(record) == "table" then
+			goThroughtTableCount = goThroughtTableCount + 1
+			print("11111111111111111111111111111", goThroughtTableCount)
+			GoThroughTable(record)
+		else
+			print(index, record)
+		end
+	end
+end
+
+local function LoginZGJCB(recordList, dbid)
+	print("function LoginZGJCB")
+	print(dbid)
+
+	for index, record in pairs(recordList) do
+		if type(record) == "table" then
+			GoThroughTable(record)
+		else
+			print("22222222222222222222222 no table")
+			print(index, record)
+		end
+	end
+	print("-------------------------------end------------------------------")
+end
+
+LuaDBAccess.LoginZGJ(LoginZGJCB, "------------------------start--------------------------------")
+
